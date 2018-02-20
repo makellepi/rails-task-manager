702239c993d430f8cce37dcd8e56e0630694e398
Marta Keller <marta.keller.pie@gmail.com>
2018-02-20 17:15:04 +0000

    task manager'

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
